4d5365edb1a409f0170748239bd7fc95843c638f
Mhe <79128545+mhereiasol@users.noreply.github.com>
2021-02-28 17:31:55 +0800

    Add files via upload

---
+++ FIzzBuzz.py
@@ -0,0 +1,38 @@
+i= 1
+while i < 100:
+	if (i % 3 is i % 5 == 0):
+		print("bizzbuzz")
+	elif (i % 3 == 0):
+		print("fizz")
+	elif (i % 5 == 0):
+		print("buzz")
+	else:
+		print(i)
+	i += 1
+	
+print("charann HAHAHAHAHHAAHAAAA")
+yan na 
+HAHAHAHAH
+natatae ako sayo
+ako din 
+kaya mo basahin yung code?
+oum
+bakit di na ako gumawa ng list of numbers?k?asi may i na
+yieee HAHAHAHA kasi 
+masakit sa ulo? hahaha
+next assignment?y?e?eeey
+ahmmmmmm calculator scoentific HAHAHAHA
+AAAAAAAAAAAgame!pero gagawa muna ako assignment ko sa module 1month na yun eh HAHAHHAAHo
+hahaha wag scientific kahit calcu lang. dapat interactive ah
+okiii 
+masaya na ako na napalabas ko kanina yung bizzbuzz HAHAHAHAHHAAHAAAAhahahahahahaha
+fizzbuzz kasi hamhahainus points ba ako HAHAHAHAHHAAHAAAAhahahahahahaha grade mo tres
+yeeeeeeeey, may bizzbuzz parin HAHAHAHAHHAAHAAAAhahahahahahahah
+ansaya kaya sa feeling HAHAHAHAHHAAHAAAAhahahahahahahah
+k-😂😂HAHAHAHAHA tulog ka na
+busog pa. maya maya 
+geh na next time ulet zzz okiiii thaaannnnnnnnkk👍👍👍👍come again HAAAAhahahahahahahaho
+pwede. take ka muna course sa pycharm hahahaha malaki kasi talaga yun oomh
+teeee
+firstnumber=float ?ctrl z mo
+yann hahahahahaha. may ginawa akong bago punta ka sa chelsua hanapin mo yung repl for calcu
